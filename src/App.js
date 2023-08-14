27cd5fa563692a59b5b8fe44781a4fbde7b225bb
IJE2704 <mh1669101@gmail.com>
2023-08-14 20:02:13 +0600

    component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Cost from './Components/Cost/Cost';
 
 function App() {
   return (
     <div className="App">
-      
+      <Cost></Cost>
     </div>
   );
 }
---
+++ src/Components/Cost/Cost.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cost = () => {
+    return (
+        <div>
+            <h1>Cost component is here</h1>
+        </div>
+    );
+};
+
+export default Cost;
